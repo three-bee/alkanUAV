a444c34ddd3ecaded677a66b224e03e450b61f0c
3b83 <54000095+3b83@users.noreply.github.com>
2021-03-06 16:44:34 +0300

    Update runSiha.sh

--- runSiha.sh
+++ runSiha.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# This script runs the drone swarm with cameras launch file, as well as each drones mavlink terminals and mavros. 
+# After 25 seconds, it will attempt to run simple_goto.py from DroneKit on 1st drone.
+
 gnome-terminal \
 --tab -t 'ROS_ArduCopter' -e 'roslaunch iq_sim multi_drone_with_camera.launch' \
 --tab -t 'Drone1' -e 'sim_vehicle.py -v ArduCopter -f gazebo-iris -I0 --console --out 127.0.0.1:14553' \
